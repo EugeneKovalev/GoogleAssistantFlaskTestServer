c3d3c82e813973f360e19dbebcdf98e50adbb74b
Eugene Kovalev <yevgeniy.kovalev@leverx.com>
2018-01-17 17:38:45 +0300

    fix

--- main.py
+++ main.py
@@ -14,14 +14,11 @@ def handle_google_assistant_request():
     action = body['result']['action']
 
     if action == 'prioritize_issue':
-        name = ''
-        description = ''
+        root_context = [_ for _ in body['result']['contexts'] if _['name'] == 'context-of-named-issue'][0]
+
+        name = root_context['parameters']['issue_name']
+        description = root_context['parameters']['issue_description']
         priority = body['result']['parameters']['issue_priority']
-        for context in body['result']['contexts']:
-            if context['name'] == 'context-of-named-issue':
-                name = context['parameters']['issue_name']
-            elif context['name'] == 'context-of-described-issue':
-                description = context['parameters']['issue_description']
 
         return jsonify({
             "speech": "The name of the {0} issue is {1}. Described as {2}".format(
@@ -34,50 +31,6 @@ def handle_google_assistant_request():
             "contextOut": body['result']['contexts'],
             "source": "testserver"
         })
-
-    # elif action == 'describe_issue':
-    #     session['description'] = body['parameters']['any']
-    #
-    #     return jsonify({
-    #         "speech": "The description of the issue is " + session['description'] + " Please, set the priority of an issue!",
-    #         "displayText": "The description of the issue is " + session['description'],
-    #         "data": {},
-    #         "contextOut": body['result']['contexts'],
-    #         "source": "testserver"
-    #     })
-
-
-
-    #
-    # if request.method == 'GET':
-    #     return 'Hello, world!'
-    # else:
-    #
-    #
-    #     if body.get('originalRequest'):
-    #         user_id = body['originalRequest']['data']['user']['userId']
-    #         conversation_id = body['originalRequest']['data']['conversation']['conversationId']
-    #     else:
-    #         user_id = 0
-    #         conversation_id = 0
-    #
-    #      # BY THIS UNIQUE VAR WE CAN DEFINE WHAT SHOULD WE DO
-    #     contexts = body['result']['contexts']  # contexts - they store data about raw user input and passed parameters
-    #
-    #     print("""
-    #     USER ID: {0} \n
-    #     CONVERSATION ID: {1} \n
-    #     ACTION: {2} \n
-    #     CONTEXTS: {3} \n
-    #     """.format(user_id, conversation_id, action, contexts))
-    #
-    #     return jsonify({
-    #         "speech": "Eugene Kovalev server speech", # text a user will hear
-    #         "displayText": "Eugene Kovalev server text", # text a user will see
-    #         "data": {}, # "data": {"facebook": {<facebook_message>}}
-    #         "contextOut": contexts, # we can change it before sending to Google
-    #         "source": "eugene's server"
-    #     })
 
 
 if __name__ == '__main__':
